8e1dbfd78892ca009a432763f5bca33127646245
Ethan Chinnasamy <ethan.chinnasamy@standardbank.co.za>
2023-12-05 13:52:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._22_Static_Methods_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+
+            UsefulTools.sayHi("Evans");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._22_Static_Methods_and_Classes
+{
+    static internal class UsefulTools
+    {
+        public static void sayHi(String name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+    }
+}
